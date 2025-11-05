42e9fc90789a72db284e7cee4de9547b32ca9065
wig <guenther.wiedemair@gmail.com>
2025-11-05 12:31:27 +0100

    Change to C++

--- build_files/cmake/platform_win32.cmake
+++ build_files/cmake/platform_win32.cmake
@@ -6,17 +6,12 @@ if (CMAKE_CL_64)
     add_definitions(-DWIN64)
 endif ()
 
-list(APPEND PLATFORM_LINK_LIBS
-        advapi32
-)
-
 string(APPEND CMAKE_Fortran_FLAGS_DEBUG " /Z7 /debug:full")
 
-string(APPEND CMAKE_C_FLAGS " /nologo /J /Gd /MP /bigobj /Zc:inline")
-
 set(CMAKE_C_COMPILER_LAUNCHER sccache)
-string(APPEND CMAKE_C_FLAGS_DEBUG " /MDd")
-string(APPEND CMAKE_C_FLAGS_RELEASE " /MD")
+string(APPEND CMAKE_C_FLAGS " /nologo /J /Gd /MP /bigobj /Zc:inline")
+string(APPEND CMAKE_C_FLAGS_DEBUG " /MDd /Wall")
+string(APPEND CMAKE_C_FLAGS_RELEASE " /MD /W1")
 
 # no default lib
 string(APPEND PLATFORM_LINKFLAGS_RELEASE " /NODEFAULTLIB:libcmt.lib /NODEFAULTLIB:libcmtd.lib /NODEFAULTLIB:msvcrtd.lib")
